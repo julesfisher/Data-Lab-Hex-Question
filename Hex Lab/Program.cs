0530c373937194dca8fc067b18d7c25fda13b455
julesfisher <91568208+julesfisher@users.noreply.github.com>
2021-10-22 17:12:55 -0400

    First Commit

---
+++ Hex Lab/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hex_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hex = "0xFE";
+            int hexint = Convert.ToInt32(hex, 16);
+            Console.WriteLine(hexint);
+        }
+    }
+}
